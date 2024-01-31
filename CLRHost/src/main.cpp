716cccac771d9c75f536835ddab7256d9150695c
fox xof <16710818+PoofImaFox@users.noreply.github.com>
2024-01-31 03:31:47 -0500

    Crossplat for main.cpp

--- CLRHost/src/main.cpp
+++ CLRHost/src/main.cpp
@@ -5,37 +5,51 @@ DLL entry point for running C#
 #include "CLR.hpp"
 
 #include <thread>
+#include <filesystem>
 
 //runs the CLR thread and DLL
 void RUNCLR()
 {
-	CLR::CLRHost host;;
-	std::basic_string<char_t> app_path = L"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Palworld\\Pal\\Binaries\\Win64\\ManagedModFramework\\PalworldManagedModFramework.dll";
+#if defined(_WIN32)
+	std::basic_string<char_t> appPath = L"Pal\\Binaries\\Win64\\ManagedModFramework\\PalworldManagedModFramework.dll";
+	auto configPath = STR("Pal\\Binaries\\Win64\\ManagedModFramework\\PalworldManagedModFramework.runtimeconfig.json");
+#elif defined(__linux__)
+	std::basic_string<char_t> appPath = L"Pal\\Binaries\\Linux\\ManagedModFramework\\PalworldManagedModFramework.dll";
+	auto configPath = STR("Pal\\Binaries\\Linux\\ManagedModFramework\\PalworldManagedModFramework.runtimeconfig.json");
+#endif
+
+	auto fullAppPath = std::filesystem::absolute(appPath).wstring();
+	auto fullConfigPath = std::filesystem::absolute(configPath).wstring();
 
 	//init the CLR
-	auto configPath = STR("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Palworld\\Pal\\Binaries\\Win64\\ManagedModFramework\\PalworldManagedModFramework.runtimeconfig.json");
-	if (!host.Init(configPath))
-	{
-		std::cout << "Failed To Init ^2\n";
+	CLR::CLRHost host;
+	if (!host.Init(fullConfigPath.c_str())) {
+		std::cout << "Failed To Init Host" << std::endl;
 		return;
 	}
-	std::cout << "Finished Initalizing CLR\n";
+
+	std::cout << "Finished Initializing CLR" << std::endl;
 
 	//starts the main Assembly
-	host.StartAssembly(app_path.c_str());
-}
+	host.StartAssembly(fullAppPath.c_str());
+	}
 
 void SpawnClrThread() {
 
-	//inits console
+	//inits console if running on windows.
+#if defined(_WIN32)
 	AllocConsole();
 	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+#endif
 
 	//starts thread and detachtes it
 	std::thread worker(RUNCLR);
 	worker.detach();
 }
 
+
+#if defined(_WIN32)
+// Windows-specific code
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  reason, LPVOID lpReserved)
 {
 	//checks the state of the DLL
@@ -57,3 +71,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  reason, LPVOID lpReserved)
 
 	return TRUE;
 }
+#elif defined(__linux__)
+// Linux-specific code
+__attribute__((constructor))
+static void DllMainCtor() {
+	SpawnClrThread();
+}
+#endif
