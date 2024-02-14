33d5bb2d91402bdfd964b5ff283efc46a00afcab
fox xof <16710818+PoofImaFox@users.noreply.github.com>
2024-02-14 15:42:49 -0500

    adding node model

---
+++ DotNetSdkBuilderMod/AssemblyBuilding/Models/CodeGenAssemblyNode.cs
@@ -0,0 +1,39 @@
+﻿namespace DotNetSdkBuilderMod.AssemblyBuilding.Models {
+    public class CodeGenAssemblyNode {
+        public CodeGenNamespaceNode[] namespaces;
+    }
+
+    public class CodeGenNamespaceNode {
+        public CodeGenNamespaceNode[] namespaces;
+        public ClassNode[] classes;
+    }
+
+    public class CodeGenClassNode {
+        public string attributes;
+
+        public string modifer;
+        public string name;
+
+        public CodeGenPropertyNode[] propertyNodes;
+        public CodeGenMethodNode[] methodNodes;
+    }
+
+    public class CodeGenPropertyNode {
+        public string attributes;
+
+        public string modifer;
+        public string returnType;
+        public string name;
+        public string get;
+        public string set;
+    }
+
+    public class CodeGenMethodNode {
+        public string attributes;
+
+        public string modifer;
+        public string returnType;
+        public string name;
+        public string arguments;
+    }
+}
