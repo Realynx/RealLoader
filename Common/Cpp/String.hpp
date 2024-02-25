16ec982cc77de74848ad7dc3ca52a9f5dcd2d994
Sean Mott <rafal.mott1@gmail.com>
2024-02-25 18:01:24 -0500

    Cleaned up comments in String.hpp and unified the formating of single line inline functions

--- Common/Cpp/String.hpp
+++ Common/Cpp/String.hpp
@@ -42,33 +42,14 @@ namespace PalMM::Util
     //defines a string literal for wide chars
 #define STR(s) s
 
-    std::string ConvertCStringToThickString(const std::string& str)
-    {
-        return str;
-        //if (str.empty()) return std::wstring();
-        //int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
-        //std::wstring wstrTo(size_needed, 0);
-        //MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-        //return wstrTo;
-    }
+    std::string ConvertCStringToThickString(const std::string& str) { return str; }
 
-    std::string ConvertThickStringToCString(const std::string& wstr)
-    {
-        return wstr;
-       //if (wstr.empty()) return std::string();
-       //int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
-       //std::string strTo(size_needed, 0);
-       //WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-       //return strTo;
-    }
+    std::string ConvertThickStringToCString(const std::string& wstr) { return wstr; }
 
 #endif
 
     //compares two char arrays
     inline bool IsSameString(const char* s1, const char* s2) { return strcmp(s1, s2); }
-
-    //compares two thick char arrays
-   // inline bool IsSameString(const wchar_t* s1, const wchar_t* s2) {return IsSameString(ConvertThickStringToCString(s1).c_str(), ConvertThickStringToCString(s2).c_str());}
 
     //defines a string that can be converted between thick and c string on the fly
     struct String
@@ -106,21 +87,13 @@ namespace PalMM::Util
 #else
 
         //sets the string data
-        inline void SetCharData(const char_t* data)
-        {
-            charData = std::string(data); // ConvertThickStringToCString(data);
-            //thickCharData = std::wstring(data);
-        }
+        inline void SetCharData(const char_t* data) { charData = std::string(data); }
 
         //sets the string data
-        inline void SetThickCharData(const char* data)
-        {
-           // thickCharData = ConvertCStringToThickString(data);
-            charData = std::string(data);
-        }
+        inline void SetThickCharData(const char* data) { charData = std::string(data); }
 
         //gets the thick char array
-        inline const char_t* GetWideCharArray() { return charData.c_str(); } //{ return thickCharData.c_str(); }
+        inline const char_t* GetWideCharArray() { return charData.c_str(); }
 
         //gets the char array
         inline const char* GetCharArray() { return charData.c_str(); }
