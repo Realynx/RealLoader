554f9a8c9c657bb312dd2eeecaf32a326885d326
Sean Mott <rafal.mott1@gmail.com>
2024-02-26 20:49:37 -0500

    Updated BuildProject bat script
    It uses dotnet build for C# now

--- BuildProject.bat
+++ BuildProject.bat
@@ -1,4 +1,4 @@
 REM compiles all the dist for C# and C++
 
 msbuild C++WindowsBuild.sln /p:Configuration=Dist /p:Platform=x64 -target:Rebuild
-msbuild PalworldManagedModFramework.sln /p:Configuration=Release -target:Rebuild
+dotnet build PalworldManagedModFramework.sln /p:Configuration=Release
