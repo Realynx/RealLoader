d65e3c386a239a0a5dfce7e7fe07160e76bd074b
Sean Mott <rafal.mott1@gmail.com>
2024-02-26 20:50:23 -0500

    fixed build to C#

--- BuildProject.bat
+++ BuildProject.bat
@@ -1,4 +1,4 @@
 REM compiles all the dist for C# and C++
 
 msbuild C++WindowsBuild.sln /p:Configuration=Dist /p:Platform=x64 -target:Rebuild
-dotnet build PalworldManagedModFramework.sln /p:Configuration=Release
+dotnet publish PalworldManagedModFramework.sln /p:Configuration=Release
