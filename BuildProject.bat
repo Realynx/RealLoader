77e30a1ec94efb3a89593a24e92f6cfcbaf54fdb
PoofImaFox <16710818+PoofImaFox@users.noreply.github.com>
2025-02-13 21:10:03 -0500

    Deleted some old files, updated cmake

--- BuildProject.bat
+++
@@ -1,5 +0,0 @@
-REM compiles all the dist for C# and C++
-
-msbuild RealLoader.sln /p:Configuration=Dist /p:Platform=x64 -target:Rebuild
-dotnet publish RealLoader.sln /p:Configuration=Release /p:PublishProfile=Release
-pause
