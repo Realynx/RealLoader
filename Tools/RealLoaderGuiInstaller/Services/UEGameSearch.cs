e261a90677bfbb7e63a028adb637eceffddf50d0
PoofImaFox <16710818+PoofImaFox@users.noreply.github.com>
2025-02-14 13:17:08 -0500

    Revert "Cleanup UEGameSearch"
    This reverts commit e96ab84a12648bcec8bc71ebc48ef863e22c3812.

--- Tools/RealLoaderGuiInstaller/Services/UEGameSearch.cs
+++ Tools/RealLoaderGuiInstaller/Services/UEGameSearch.cs
@@ -1,35 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection.PortableExecutable;
-using System.Runtime.Versioning;
+using System.Threading.Tasks;
 
 using Microsoft.Win32;
 
 namespace RealLoaderGuiInstaller.Services {
     internal static class UEGameSearch {
-        public static string[] FindUEGames() {
+        public static unsafe string[] FindUEGames() {
             // TODO: make this cross plat compatible, this will prbly require DI services and interfaces.
             var installedWindowsSoftware = GetPotentialUEPaths();
 
             var ueGames = new List<string>();
             foreach (var softwareDir in installedWindowsSoftware) {
-                var foundExecutables = Directory.EnumerateFiles(softwareDir, "*.exe").ToArray();
-                if (foundExecutables.Length != 1) {
+                var foundExecutables = Directory.EnumerateFiles(softwareDir, "*.exe");
+                if (foundExecutables.Count() != 1) {
                     continue;
                 }
 
                 var aliasExecutable = foundExecutables.Single();
-                //var UEProjectName = aliasExecutable.Split(".")[0];
+                var UEProjectName = aliasExecutable.Split(".")[0];
 
-                // TODO: Not all UE games end with "Win64-Shipping.exe" e.g. Rocket League
-                var enumerationOptions = new EnumerationOptions { RecurseSubdirectories = true };
-                var win64Path = Directory.EnumerateFiles(softwareDir, "*", enumerationOptions)
+                var win64Path = Directory.EnumerateFiles(softwareDir, "*", SearchOption.AllDirectories)
                     .SingleOrDefault(i => i.EndsWith("Win64-Shipping.exe"));
 
                 if (win64Path is null || !File.Exists(win64Path)) {
                     continue;
                 }
+
+
 
                 using var aliasExecutableStream = File.OpenRead(aliasExecutable);
                 using var win64PathStream = File.OpenRead(win64Path);
@@ -47,8 +48,12 @@ namespace RealLoaderGuiInstaller.Services {
             return [.. ueGames];
         }
 
-        private static bool ContainsFingerprint(FileStream fileStream, string dataSearchSection) {
-            var fingerPrint = "U\0n\0r\0e\0a\0l\0E\0n\0g\0i\0n\0e\0"u8;
+        private static unsafe bool ContainsFingerprint(FileStream fileStream, string dataSearchSection) {
+            ReadOnlySpan<byte> fingerPrint = [
+                0x00, 0x55, 0x00, 0x6E, 0x00, 0x72, 0x00, 0x65,
+                0x00, 0x61, 0x00, 0x6C, 0x00, 0x45, 0x00, 0x6E,
+                0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x65, 0x00
+            ];
 
             var peReader = new PEReader(fileStream);
             var rdataSection = peReader.GetSectionData(dataSearchSection);
@@ -72,15 +77,14 @@ namespace RealLoaderGuiInstaller.Services {
             return false;
         }
 
-        [SupportedOSPlatform("windows")]
-        private static string[] GetPotentialUEPaths() {
-            var installPaths = new HashSet<string>();
+        static string[] GetPotentialUEPaths() {
+            var installPaths = new List<string>();
 
-            // TODO: When users relocate a game through steam, the uninstaller reg key is not updated
-            string[] registryKeys = [
+            string[] registryKeys =
+            {
                 @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
-                @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall",
-            ];
+                @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+            };
 
             foreach (var keyPath in registryKeys) {
                 using var key = Registry.LocalMachine.OpenSubKey(keyPath);
@@ -103,7 +107,7 @@ namespace RealLoaderGuiInstaller.Services {
                 }
             }
 
-            return installPaths.ToArray();
+            return installPaths.Distinct().ToArray();
         }
     }
 }
