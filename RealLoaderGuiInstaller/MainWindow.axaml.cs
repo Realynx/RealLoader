267cbf14a3073256ce7ec24eefb6bfc6ae7e2a17
PoofImaFox <16710818+PoofImaFox@users.noreply.github.com>
2025-02-14 13:17:08 -0500

    Revert "Updating pipelines"
    This reverts commit 96049736e745e6b10289db4a667946e3b638f65e.

--- RealLoaderGuiInstaller/MainWindow.axaml.cs
+++ RealLoaderGuiInstaller/MainWindow.axaml.cs
@@ -1,58 +1,9 @@
-using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
-
 using Avalonia.Controls;
 
-using DynamicData;
-
-using ReactiveUI;
-
-using RealLoaderGuiInstaller.Services;
-
 namespace RealLoaderGuiInstaller {
-    public partial class MainWindow : Window, IReactiveObject {
-        [DllImport("kernel32.dll")]
-        private static extern bool AllocConsole();
-
-        private string _selectedGamePath;
-
-        public event PropertyChangingEventHandler? PropertyChanging;
-
-        public ObservableCollection<string> AvailableGamePaths { get; } = new();
-
-        public string SelectedGamePath {
-            get => _selectedGamePath;
-            set => this.RaiseAndSetIfChanged(ref _selectedGamePath, value);
-        }
-
+    public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-
-            var installedGames = UEGameSearch.FindUEGames();
-            AvailableGamePaths.AddRange(installedGames);
-
-            DataContext = this;
-        }
-
-        public void Install(string gamePath) {
-            AllocConsole();
-
-            Console.WriteLine($"Installing RealLoader to '{gamePath}'");
-            RealLoaderInstaller.Program.Main(["-l", $"{gamePath}"]);
-        }
-
-        public void InstallButton() {
-            Install(SelectedGamePath);
-        }
-
-        public void RaisePropertyChanging(PropertyChangingEventArgs args) {
-            throw new System.NotImplementedException();
-        }
-
-        public void RaisePropertyChanged(PropertyChangedEventArgs args) {
-            throw new System.NotImplementedException();
         }
     }
 }
